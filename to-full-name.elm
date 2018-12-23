ea85c1fefd6c9ad1dbb40669b8046eaeb557a96b
mehdivk <hi@mv.id.au>
2018-12-23 19:40:41 +1100

    toFullName

--- to-full-name.elm
+++ to-full-name.elm
@@ -2,4 +2,4 @@ toFullName person =
   person.firstName ++ " " ++ person.lastName
 
 fullName =
-  toFullName { fistName = "Mehdi", lastName =  "Valikhani" }
+  toFullName { firstName = "Mehdi", lastName =  "Valikhani" }
